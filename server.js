6f693bd917f3e345bfdb9b291395912925238fdf
grp0060078 <grp006007@gmail.com>
2023-11-16 08:44:27 +0530

    notes add

--- server.js
+++ server.js
@@ -4,11 +4,29 @@
 
  const port = 3001;
 
+ let notes = [
+    {
+        id: 1,
+        content: 'backend using node.js',
+        important: true
+    },
+    {
+        id: 2,
+        content: 'node.js is a open source',
+        important: false,
+    },
+    {
+        id: 3,
+        content: 'simple web server using node.js',
+        important: true
+    }
+];
+
  const  server = http.createServer((req,res) =>{
       
      res.statusCode=200;
-     res.setHeader('content_type', 'text/plain')
-     res.end("hello World\n")
+     res.setHeader('content_type', 'application/json')
+     res.end(JSON.stringify(notes))
  } );
 
  server.listen(port,hostname,() =>{
