f3f35ecf84eb78e3dec482802677f3eb81ec40b0
grp0060078 <grp006007@gmail.com>
2023-11-16 08:17:32 +0530

    a simple web browser

---
+++ server.js
@@ -0,0 +1,16 @@
+ const http =require ("http")
+
+ const hostname ="127.0.0.1";  
+
+ const port = 3001;
+
+ const  server = http.createServer((req,res) =>{
+      
+     res.statusCode=200;
+     res.setHeader('content_type', 'text/plain')
+     res.end("hello World\n")
+ } );
+
+ server.listen(port,hostname,() =>{
+    console.log(`server running at http://${hostname}:${port}`)
+ })
